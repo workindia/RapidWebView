ec75c824104baf3c0b1dfb47feb477c22a016a17
Kshitij <ksnagvekar@gmail.com>
2022-01-25 20:13:49 +0530

    Update app link

--- samples/RapidWebViewSdkDocsSite/pages/examples/index.tsx
+++ samples/RapidWebViewSdkDocsSite/pages/examples/index.tsx
@@ -26,7 +26,7 @@ const Examples: NextPage = () => {
               </Text>
               <Button
                 as="a"
-                href="https://appsenjoy.com/DrWdn"
+                href="https://appsenjoy.com/r2c3F"
                 target={"_blank"}
                 colorScheme={"green"}
                 mr="3"
