e10dfc3b654d8f7a0be4964e9b090dd5189b2d80
Mandeep Singh <mandeep.singh@workindia.in>
2022-04-04 18:51:21 +0530

    documentation updated

--- samples/RapidWebViewSdkDocsSite/pages/docs/js-interface/functions.tsx
+++ samples/RapidWebViewSdkDocsSite/pages/docs/js-interface/functions.tsx
@@ -23,6 +23,88 @@ const FUNCTION_DOCS = [
       "Show native toast. Takes text to be shown in the toast and duration (0 for short and 1 for long duration).",
     code: '.toast("Hello World", 0)',
   },
+  {
+    id: 3,
+    title: "Copy To Clipboard",
+    description:
+      "Copy text to the clipboard. It needs only one function parameter, the text to be copied.",
+    code: '.copyToClipboard("Sample Text")',
+  },
+  {
+    id: 4,
+    title: "Get installed apps list",
+    description:
+      "It returns the list of package name of installed apps.",
+    code: '.getInstalledAppList()',
+  },
+  {
+    id: 5,
+    title: "Start an app activity",
+    description:
+      "Start an activity within the application. It takes 2 parameters, activity name(class name of the activity to navigate to) and intent parameters(json of parameters to be passed to activity in string format).",
+    code: '.startActivity("in.workindia.rapidwebviewandroidsample.MainActivity", "{"key1": 1, "key2": "value2"}")',
+  },
+  {
+    id: 6,
+    title: "Open Browser",
+    description:
+      'Open external browser as a new window. It takes two paramters, url(complete url to visit) and package name(name of browser package to open eg: "com.android.chrome").',
+    code: '.openBrowserActivity("https://www.google.com/", "com.android.chrome")',
+  },
+  {
+    id: 7,
+    title: "Open Chrome Custom Tab",
+    description: 'To open a chrome custom tab. It takes two parameters, url(complete url to visit) and color(Chrome custom tab color). Color in the form of string eg.) "#483D8B", "#80FFFFFF" and "red".',
+    code: '.openCustomBrowserTab("https://www.google.com/", "#483D8B")',
+  },
+  {
+    id: 8,
+    title: "Place a call / Open dialer",
+    description: 'Open a dialer (and call a number if permission is granted). It only requires one paramter the mobile number in the form of string.',
+    code: '.openDialer("1234567890")',
+  },
+  {
+    id: 9,
+    title: "Open an external intent (eg, google maps)",
+    description: 'To open a new application. It requires two parameters, package name(name of package to open), and intent uri.',
+    code: '.openExternalIntent("com.google.android.gms.maps", "geo:37.7749,-122.4194")',
+  },
+  {
+    id: 10,
+    title: "Open native share dialog",
+    description: 'To open android\'s native share dailog. It requires only one paramter, the text to be shared.',
+    code: '.openShareIntent("Try RapidWebView SDK: https://github.com/workindia/RapidWebView")',
+  },
+  {
+    id: 11,
+    title: "Share to an application (eg, slack)",
+    description: 'Share text with a specific app. It requires two paramters, the package name of the app with which text is need to be shared, and second paramter the text to be shared.',
+    code: '.shareToApp("com.Slack", "Checkout this SDK: https://github.com/workindia/RapidWebView")',
+  },
+  {
+    id: 12,
+    title: "Check if permissions granted (eg, android.permission.CALL_PHONE)",
+    description: 'Check if permission is granted by user. It requires only one paramter, the array of permissions to check.',
+    code: '.checkForPermission([ "android.permission.CALL_PHONE"])',
+  },
+  {
+    id: 13,
+    title: "Request permission",
+    description: 'To ask for android permission. It requires 3 parameters, an array containing all the permissions needed, rationale Text(text to show on permission dialog) and Integer request-code passed for callback in the form of string.',
+    code: '.requestPermissions(["android.permission.CALL_PHONE"], "Permission required",null)',
+  },
+  {
+    id: 14,
+    title: "Show notification",
+    description: 'To show notification to user. It requires 6 parameters, title for the notification, context text, summary text, notification icon, notification image, destination activity(activity to open when notification is clicked).',
+    code: '.showNotification("Test Notification","Test Content","Test Summary","","","in.workindia.rapidwebviewandroidsample.MainActivity"',
+  },
+  {
+    id: 15,
+    title: "Close current activity",
+    description: 'Close the activity that holds the web view.',
+    code: '.closeActivity()',
+  },
 ];
 
 const FunctionDocItem = (item: {
@@ -88,9 +170,6 @@ const JSInterfaceFunctions: NextPage = () => {
                 <FunctionDocItem key={item.id} {...item} />
               ))}
 
-              <Text>
-                {"// TODO: Rest of the functions will be documented soon."}
-              </Text>
             </Box>
           </SidebarLayout>
         </main>
