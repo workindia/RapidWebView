1570a67666475e049799efa9aaff6462fd270c35
@krishna-kudari <kkrishna22572@gmail.com>
2024-09-19 11:32:35 +0530

    remove comments

--- RapidWebView/src/main/java/in/workindia/rapidwebview/datamodels/DownloadLocation.kt
+++ RapidWebView/src/main/java/in/workindia/rapidwebview/datamodels/DownloadLocation.kt
@@ -1,6 +1,6 @@
 package `in`.workindia.rapidwebview.datamodels
 
 enum class DownloadLocation {
-    EXTERNAL_FILES,   // shared but app-specific
-    PUBLIC_DOWNLOADS  // public download directory accessible to all apps
+    EXTERNAL_FILES,
+    PUBLIC_DOWNLOADS
 }
