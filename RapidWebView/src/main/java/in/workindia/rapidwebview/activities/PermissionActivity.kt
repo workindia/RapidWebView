32ef6427e4ebeb9df9e1373884a05169d2d79cfd
@krishna-kudari <kkrishna22572@gmail.com>
2024-09-19 11:41:33 +0530

    remove appendPendingDataToIntent [permissions to be handled in javascript]

--- RapidWebView/src/main/java/in/workindia/rapidwebview/activities/PermissionActivity.kt
+++ RapidWebView/src/main/java/in/workindia/rapidwebview/activities/PermissionActivity.kt
@@ -6,12 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import `in`.workindia.rapidwebview.R
 import `in`.workindia.rapidwebview.constants.BroadcastConstants
-import `in`.workindia.rapidwebview.constants.BroadcastConstants.Companion.DOWNLOAD_LOCATION
-import `in`.workindia.rapidwebview.constants.BroadcastConstants.Companion.DOWNLOAD_URL
-import `in`.workindia.rapidwebview.constants.BroadcastConstants.Companion.FILE_NAME
-import `in`.workindia.rapidwebview.constants.BroadcastConstants.Companion.PERMISSION_LIST_INTENT
+import `in`.workindia.rapidwebview.constants.BroadcastConstants.Companion.PERMISSION_LIST_KEY
 import `in`.workindia.rapidwebview.constants.BroadcastConstants.Companion.RATIONAL_TEXT
-import `in`.workindia.rapidwebview.utils.Utils
 import pub.devrel.easypermissions.EasyPermissions
 import pub.devrel.easypermissions.EasyPermissions.requestPermissions
 
@@ -26,7 +22,7 @@ class PermissionActivity : AppCompatActivity(), EasyPermissions.PermissionCallba
 
         val intent = intent
 
-        permissions = intent?.getStringArrayExtra(PERMISSION_LIST_INTENT) as Array<String>
+        permissions = intent?.getStringArrayExtra(PERMISSION_LIST_KEY) as Array<String>
         val rationalText = intent?.getStringExtra(RATIONAL_TEXT) ?: "This permission is required"
 
         if (permissions.isEmpty()) {
@@ -50,25 +46,10 @@ class PermissionActivity : AppCompatActivity(), EasyPermissions.PermissionCallba
 
         intentBroadcast.putExtra(BroadcastConstants.PERMISSION, status)
         intentBroadcast.putExtra(BroadcastConstants.PERMISSION_LIST, permissions)
-        appendPendingDataToIntent(intentBroadcast)
 
         LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intentBroadcast)
 
         finish()
-    }
-
-    private fun appendPendingDataToIntent(intentBrodCast: Intent) {
-        val bundle = intent.extras
-        val url: String? = bundle?.getString(DOWNLOAD_URL)
-        val fileName: String? = bundle?.getString(FILE_NAME)
-        val downloadLocationName: String =
-            Utils.toDownloadLocation(bundle?.getString(DOWNLOAD_LOCATION)).name
-
-        intentBrodCast.putExtras(Bundle().apply {
-            putString(DOWNLOAD_URL, url)
-            putString(FILE_NAME, fileName)
-            putString(DOWNLOAD_LOCATION, downloadLocationName)
-        })
     }
 
     override fun onRequestPermissionsResult(
