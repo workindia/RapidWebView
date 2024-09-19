be15b70f9bf832323f48bc7536cb7959dedfbc71
@krishna-kudari <kkrishna22572@gmail.com>
2024-09-19 11:40:02 +0530

    clean Utils.kt

--- RapidWebView/src/main/java/in/workindia/rapidwebview/utils/Utils.kt
+++ RapidWebView/src/main/java/in/workindia/rapidwebview/utils/Utils.kt
@@ -6,15 +6,6 @@ import `in`.workindia.rapidwebview.datamodels.DownloadLocation
 object Utils {
 
     /**
-     * Check url is a valid string or not and run [block] if not
-     */
-    inline fun validateUrl(url: String?, block: () -> Unit) {
-        if (url.isNullOrBlank()) {
-            block()
-        }
-    }
-
-    /**
      *  @return fileName if valid String otherwise return lastPathSegment of uri
      */
     fun getFileNameFromUri(fileName: String?, uri: Uri): String {
@@ -29,13 +20,9 @@ object Utils {
      *  @return [DownloadLocation] returns matching value from [DownloadLocation] enum.
      *  Default value is [DownloadLocation.PUBLIC_DOWNLOADS]
      */
-    fun toDownloadLocation(location: String?): DownloadLocation {
+    fun toDownloadLocation(location: String): DownloadLocation {
         return try {
-            if (location != null) {
-                DownloadLocation.valueOf(location)
-            } else {
-                DownloadLocation.PUBLIC_DOWNLOADS
-            }
+            DownloadLocation.valueOf(location)
         } catch (e: IllegalArgumentException) {
             DownloadLocation.PUBLIC_DOWNLOADS
         }
